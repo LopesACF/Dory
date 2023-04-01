a6ba0b13d654421c98882090d6fe77039319b8cd
LopesACF <altemilsonlopes@gmail.com>
2023-04-01 13:02:38 -0300

    Dory

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Converse com a Dory!");
+
+string texto = Console.ReadLine()!;
+
+texto = texto.Replace("a", "aaa");
+texto = texto.Replace("e", "eee");
+texto = texto.Replace("i", "iii");
+texto = texto.Replace("o", "ooo");
+texto = texto.Replace("u", "uuu");
+
+Console.WriteLine("\nEm baleiês: " + texto);
